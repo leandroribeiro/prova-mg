c511a3c6b845617a1e9869dd091f5ee0a2ec38f1
Leandro Ribeiro <github@leandroribeiro.com>
2020-10-07 23:22:16 -0300

    Script facilitador de banco de dados, versão Linux

--- ProvaMG.Database/run.sh
+++ ProvaMG.Database/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CONTAINER_NAME="provamg-db"
 SA_PASSWORD="Dev123456789"
 
 echo ""
@@ -15,8 +16,25 @@ echo "O script está executando do diretório:"
 pwd
 
 echo ""
-echo "Parando containers em execução..."
-docker stop $(docker ps -q)
+
+CONTAINER_ID=$(docker container ls --all --quiet --filter "name=$CONTAINER_NAME")
+
+if [ -z $CONTAINER_ID ] 
+then
+   echo ""
+
+else
+
+   echo "Parando container"
+   docker container stop $CONTAINER_ID
+
+   sleep 5s
+
+   # echo "Removendo container"
+   # docker container rm --force $CONTAINER_ID
+
+fi
+
 
 echo ""
 echo "Subindo novo container..."
@@ -24,10 +42,11 @@ docker run -e "ACCEPT_EULA=Y" \
    -e "MSSQL_SA_PASSWORD=$SA_PASSWORD" \
    -e "SA_PASSWORD=$SA_PASSWORD" \
    -e "MSSQL_PID=Developer" \
-   --name "provamg-db" \
+   --name $CONTAINER_NAME \
    --rm \
    -p 1401:1433 \
    -d microsoft/mssql-server-linux:latest
+
 
 echo ""
 echo "Copiando arquivos para o container..."
@@ -35,22 +54,31 @@ docker cp setup.sql provamg-db:/home
 docker cp usuarios.csv provamg-db:/home
 docker cp municipios.csv provamg-db:/home
 
+
 echo ""
 echo "Dê um tempo para o BD subir okay?"
-Start-Sleep -s 5
+sleep 30s
+
 
 echo ""
 echo "Testando Conexão..."
-docker exec -it provamg-db /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U SA -P "$SA_PASSWORD" -Q "SELECT Name FROM sys.Databases"
+docker exec -it $CONTAINER_NAME /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U SA -P $SA_PASSWORD -Q "SELECT Name FROM sys.Databases"
+
 
 echo ""
 echo "Criando estrutura..."
-docker exec -it provamg-db /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U SA -P "$SA_PASSWORD" -d master -i /home/setup.sql
+docker exec -it $CONTAINER_NAME /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U SA -P $SA_PASSWORD -d master -i /home/setup.sql
+
 
 echo ""
 echo "Importando dados..."
-docker exec -it provamg-db /opt/mssql-tools/bin/bcp ProvaMG.dbo.tb_usuario in "/home/usuarios.csv" -c -t ',' -S localhost,1433 -U sa -P "$SA_PASSWORD"
-docker exec -it provamg-db /opt/mssql-tools/bin/bcp ProvaMG.dbo.tb_tipo_municipio in "/home/municipios.csv" -c -t ',' -S localhost,1433 -U sa -P "$SA_PASSWORD"
+docker exec -it $CONTAINER_NAME /opt/mssql-tools/bin/bcp ProvaMG.dbo.tb_usuario in "/home/usuarios.csv" -c -t ',' -S localhost,1433 -U sa -P $SA_PASSWORD
+docker exec -it $CONTAINER_NAME /opt/mssql-tools/bin/bcp ProvaMG.dbo.tb_tipo_municipio in "/home/municipios.csv" -c -t ',' -S localhost,1433 -U sa -P $SA_PASSWORD
 
 echo ""
-echo "*** Tudo pronto =) ***"
+echo "Testando Conexão..."
+docker exec -it $CONTAINER_NAME /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U SA -P $SA_PASSWORD -Q "SELECT Name FROM sys.Databases"
+
+echo ""
+echo "Tudo pronto =)"
+echo ""
