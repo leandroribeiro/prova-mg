746384ad9d1ad6f7f447e156a79e2c407dcfb3fc
Leandro Ribeiro <github@leandroribeiro.com>
2020-12-06 21:37:57 -0300

    Ajustando script de inicializacao

--- ProvaMG.Database/initialize-database.sh
+++ ProvaMG.Database/initialize-database.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
+DBHOST=localhost;
+DBPORT=1433;
 DBNAME=ProvaMG;
 
 echo "Dando um tempo para o banco subir =\ MSSQL tem essas coisas ...";
-sleep 60s;
+sleep 30s;
 
 echo "1/4 ) Criando estrutura do banco de dados";
-/opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P $SA_PASSWORD -i /home/setup.sql;
+/opt/mssql-tools/bin/sqlcmd -S $DBHOST,$DBPORT -U sa -P $SA_PASSWORD -i /home/setup.sql;
 
 echo "2/4 ) Consultando os bancos de dados";
-/opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P $SA_PASSWORD -Q "SELECT Name FROM sys.Databases";
+/opt/mssql-tools/bin/sqlcmd -S $DBHOST,$DBPORT -U sa -P $SA_PASSWORD -Q "SELECT Name FROM sys.Databases";
 
-# DBEXISTS=$(/opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P $SA_PASSWORD -Q "SELECT Name FROM sys.Databases WHERE [Name] LIKE '%$DBNAME%'" | grep "$DBNAME" > "1"; echo "0")
+DBEXISTS=$(/opt/mssql-tools/bin/sqlcmd -S $DBHOST,$DBPORT -U sa -P $SA_PASSWORD -Q "SELECT Name FROM sys.Databases WHERE [Name] LIKE '%$DBNAME%'" | grep "$DBNAME")
 
-# if [ $DBEXISTS -eq 1 ];then
 
-  # DATAEMPTY=$(/opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P $SA_PASSWORD -Q "SELECT COUNT(*) FROM $DBNAME.dbo.tb_usuario" | grep "0" > echo "1" ; echo "0")
-
-  # if [ $DATAEMPTY -eq 1 ];then  
+while [ -z "$DBEXISTS" ]
+do
+    sleep 10s;
+    echo "";
+    echo "Tentativa de Conexao...";
     
-    echo "3/4 Carregando massa de dados";
-    /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P $SA_PASSWORD -Q "TRUNCATE table $DBNAME.dbo.tb_usuario";
-    /opt/mssql-tools/bin/bcp $DBNAME.dbo.tb_usuario in "/home/usuarios.csv" -c -t ',' -S localhost,1433 -U sa -P $SA_PASSWORD;
+    DBEXISTS=$(/opt/mssql-tools/bin/sqlcmd -S $DBHOST,$DBPORT -U sa -P $SA_PASSWORD -Q "SELECT Name FROM sys.Databases WHERE [Name] LIKE '%$DBNAME%'" | grep "$DBNAME")
+     
+done
+
+
+if [ -n "$DBEXISTS" ]
+then
+  echo "";
+  echo "3/4 Carregando Usuarios";
+  /opt/mssql-tools/bin/sqlcmd -S $DBHOST,$DBPORT -U sa -P $SA_PASSWORD -Q "TRUNCATE table $DBNAME.dbo.tb_usuario";
+  /opt/mssql-tools/bin/bcp $DBNAME.dbo.tb_usuario in "/home/usuarios.csv" -c -t ',' -S $DBHOST,$DBPORT -U sa -P $SA_PASSWORD;
+
+  echo "";
+  echo "4/4 Carregando Municipios";
+  /opt/mssql-tools/bin/sqlcmd -S $DBHOST,$DBPORT -U sa -P $SA_PASSWORD -Q "TRUNCATE table $DBNAME.dbo.tb_tipo_municipio";
+  /opt/mssql-tools/bin/bcp $DBNAME.dbo.tb_tipo_municipio in "/home/municipios.csv" -c -t ',' -S $DBHOST,$DBPORT -U sa -P $SA_PASSWORD;
   
-    echo "4/4 Carregando massa de dados";
-    /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P $SA_PASSWORD -Q "TRUNCATE table $DBNAME.dbo.tb_tipo_municipio";
-    /opt/mssql-tools/bin/bcp $DBNAME.dbo.tb_tipo_municipio in "/home/municipios.csv" -c -t ',' -S localhost,1433 -U sa -P $SA_PASSWORD;
-    
-  # else
-    # echo "4/4 Dados já estão carregados!!"
-    
-  # fi
-   
-# fi
+fi
